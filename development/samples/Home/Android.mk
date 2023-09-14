e4d7bedf250e097c041d391a7cc59d4c9abfdc1d
lrf-ubuntu2004-m5 <1329695295@qq.com>
2023-09-14 03:14:04 -0700

    删除默认桌面的 Home Sample

--- development/samples/Home/Android.mk
+++ development/samples/Home/Android.mk
@@ -1,14 +1,14 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+# LOCAL_PATH:= $(call my-dir)
+# include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := samples
+# LOCAL_MODULE_TAGS := samples
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+# LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_PACKAGE_NAME := Home
+# LOCAL_PACKAGE_NAME := Home
 
-LOCAL_SDK_VERSION := current
+# LOCAL_SDK_VERSION := current
 
-LOCAL_DEX_PREOPT := false
+# LOCAL_DEX_PREOPT := false
 
-include $(BUILD_PACKAGE)
+# include $(BUILD_PACKAGE)
