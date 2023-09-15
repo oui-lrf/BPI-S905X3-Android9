0627f014dfcff4959c6a1fc895a93cc44dafe33e
lrf-ubuntu2004-m5 <1329695295@qq.com>
2023-09-15 08:04:49 -0700

    fix start.sh

--- device/bananapi/m5_mbox/start/start.sh
+++ device/bananapi/m5_mbox/start/start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "start,find all sh file to run"
-for script in $(ls ./*.sh); do
+# 获取当前脚本的目录
+DIR="$(dirname "$(realpath "$0")")"
+
+echo "start, find all sh files in $DIR to run"
+
+for script in $(ls "$DIR"/*.sh); do
     if [ "$script" != "$0" ]; then  # 避免脚本自身被再次执行
         echo "Executing $script..."
-        sh $script &
+        sh "$script" &
     fi
 done
-
-
