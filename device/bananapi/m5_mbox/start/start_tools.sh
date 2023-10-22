30042dc346a6b9aaa71f24d97c2fe9f0aba9ca69
lrf-ubuntu2004-m5 <1329695295@qq.com>
2023-10-21 23:21:52 -0700

    关闭adb，开启telnet

--- device/bananapi/m5_mbox/start/start_tools.sh
+++ device/bananapi/m5_mbox/start/start_tools.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# echo "start telnet!!"
-# busybox telnetd -l /system/bin/secure_shell
+echo "start telnet!!"
+busybox telnetd -l /system/bin/secure_shell
 
 sleep 10
+
 echo "stop adbd!!"
 stop adbd
 
